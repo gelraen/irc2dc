b5308a4c5c56179d9f7ef27a68853f381ce3a2bf
Maxim Ignatenko <gelraen.ua@gmail.com>
2009-05-14 11:17:28 +0000

    Replace license

--- src/config.cpp
+++ src/config.cpp
@@ -1,5 +1,5 @@
 /*-
- *   Copyright (C) 2008 by gelraen
+ *   Copyright (C) 2008-2009 by Maxim Ignatenko
  *   gelraen.ua@gmail.com
  *
  *   All rights reserved.                                                  *
@@ -13,9 +13,6 @@
  *       notice, this list of conditions and the following disclaimer in   *
  *       the documentation and/or other materials provided with the        *
  *       distribution.                                                     *
- *     * Neither the name of the gelraen nor the names of its contributors *
- *       may be used to endorse or promote products derived from this      *
- *       software without specific prior written permission.               *
  *                                                                         *
  *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   *
  *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     *
